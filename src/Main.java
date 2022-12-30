1f4b3e08db24059d5ede26c6ba3f7d2e3c08f35b
YayCover9000 <yassuanf@googlemail.com>
2022-12-30 16:12:29 +0100

    ausgabe bis Zahl

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int naechsteZahl = sc.nextInt();
+        int aufruf1 = Main.Hanoi(naechsteZahl);
+        System.out.println(naechsteZahl);
+
+    }public static int Hanoi(int a) {
+        int zahl = a;
+        int[] array = new int[zahl];
+        for(int i = 0; i < zahl; i++) {
+            array[i] = i;
+            System.out.println(array[i]);
+        }
+        return 0;
     }
+
 }
